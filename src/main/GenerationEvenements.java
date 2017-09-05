a152852f9ed393ad8169b380b77cfd17fd4797c5
valentin lucas <valentin.lucas@etudiant.univ-lille1.fr>
2017-09-05 10:42:19 +0200

    ajout des evenements de la 2e semaine

--- src/main/GenerationEvenements.java
+++ src/main/GenerationEvenements.java
@@ -31,6 +31,29 @@ public class GenerationEvenements {
 	evenements.add(ev9);
 	evenements.add(ev10);
 	
+	
+	Evenement ev11 = new Evenement("Amphi analyse","J'y vais !","J'y vais pas",0,10,-10,10,10,-10,new Date(2,1,8));
+	Evenement ev12 = new Evenement("Interro surprise","J'avais relu","Oups!",0,0,0,0,10,-10,new Date(2,2,8));
+	Evenement ev13 = new Evenement("Pause Midi, Lan en salle tp","Je me depeche de manger pour aller jouer a Urbanterror!","Je me depeche de manger pour bosser",10,-10,0,0,-10,10,new Date(2,2,12));
+	Evenement ev14 = new Evenement("Grasse mat'?","Oui","Non",0,0,10,-10,-10,10,new Date(2,3,6));
+	Evenement ev15 = new Evenement("DS d'algo","J'ai pas révisé je triche","J'ai pas révisé j'y vais au talent",0,0,0,0,-100,-25,new Date(2,3,13));
+	Evenement ev16 = new Evenement("Pause Midi","Sandwich ?","Box de pâtes ?",0,0,10,20,0,-5,new Date(2,4,12));
+	Evenement ev17 = new Evenement("Soiréé étudiante","Je sors","Je sors pas",20,-20,-20,20,-15,20,new Date(2,4,20));
+	Evenement ev18 = new Evenement("Amphi sysème avec Mr Beaufils","Je discute avec mon voisin","J'ecoute sagement",0,0,0,0,-100,30,new Date(2,5,10));
+	Evenement ev19 = new Evenement("Révisions","Je revise seul","Je revise avec un pote","Je revise pas",0,0,0,-10,-5,10,20,-10,-10,new Date(2,6,10));
+	Evenement ev20 = new Evenement("Mon pote me propose une aprem fifa","J'y vais","J'y vais pas",-10,10,0,0,0,-10,new Date(2,7,14));
+
+	
+	evenements.add(ev11);
+	evenements.add(ev12);
+	evenements.add(ev13);
+	evenements.add(ev14);
+	evenements.add(ev15);
+	evenements.add(ev16);
+	evenements.add(ev17);
+	evenements.add(ev18);
+	evenements.add(ev19);
+	evenements.add(ev20);
 	}
 	
 }
