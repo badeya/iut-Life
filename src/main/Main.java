223c74f487ffe9d90a3e306c93cd1a1abf913286
Quentin Agneray <quentin.agneray@etudiant.univ-lille1.fr>
2017-09-05 08:50:24 +0200

    creation evenement -> main()

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -8,6 +10,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		List e1 = new ArrayList<Evenement>();
 		Barre b1=new Barre("Resultat");
 		System.out.println(b1);
 
