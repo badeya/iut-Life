af09c065cdd4ea1689829b496dd76a389c60340b
Quentin Agneray <quentin.agneray@etudiant.univ-lille1.fr>
2017-09-05 08:21:31 +0200

    init Class Date

---
+++ src/main/Date.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Date {
+	private String semaine;
+	private String jour;
+	private String heure;
+	
+	public Date(String semaine, String jour, String heure){
+		this.semaine=semaine;
+		this.jour=jour;
+		this.heure=heure;
+	}	
+	
+	public String getSemaine() {
+		return semaine;
+	}
+
+
+	public String getJour() {
+		return jour;
+	}
+
+
+	public String getHeure() {
+		return heure;
+	}
+
+
+	public String toString(){
+		return "semaine:"+this.semaine+"jour:"+this.jour+"heure:"+this.heure;
+	}
+
+}
